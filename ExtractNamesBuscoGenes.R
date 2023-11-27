fdba0ba190662526127c76a4e712bc8fd0a329e3
Tristan Cumer <tristan.cumer@gmail.com>
2023-11-27 16:38:35 +0100

    Update ExtractNamesBuscoGenes.R

--- ExtractNamesBuscoGenes.R
+++ ExtractNamesBuscoGenes.R
@@ -8,7 +8,7 @@ library(jsonlite)
 
 #### Prepare the data ####
 
-## 1. download the data
+## 1. download the data for the BUSCO genes for birds
 # data for aves can be found here : https://busco-data.ezlab.org/v5/data/lineages/aves_odb10.2021-02-19.tar.gz
 # a list of all possible DB can be found here : https://busco-data.ezlab.org/v5/data/lineages/
 
@@ -17,6 +17,12 @@ library(jsonlite)
 # Then extract the file of interest :
 # tar zxvf aves_odb10.2021-02-19.tar.gz aves_odb10/links_to_ODB10.txt
 
+## 2. download the annotation infromations from NCBI
+# mkdir 0_DATA
+# wget -P 0_DATA https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/018/691/265/GCF_018691265.1_T.alba_DEE_v4.0/GCF_018691265.1_T.alba_DEE_v4.0_feature_table.txt.gz
+# gunzip 0_DATA/GCF_018691265.1_T.alba_DEE_v4.0_feature_table.txt.gz
+# awk '$1=="gene"' 0_DATA/GCF_018691265.1_T.alba_DEE_v4.0_feature_table.txt > 0_DATA/GCF_018691265.1_T.alba_DEE_v4.0_feature_table_genes.txt
+
 #### Run the R code to extract the name of the Busco Gene in the chicken "Gallus gallus"  ####
 
 # Load the file
@@ -24,15 +30,35 @@ TableBusco = read.table("aves_odb10/links_to_ODB10.txt", sep = "\t", quote = "")
 Busco_ids = TableBusco[,1]
 
 # Loop over Busco IDs to extract the matching ID's in the Gallus gallus assembly
-GenesNames = c()
+# GenesNames = c()
+# for(Busco_id in Busco_ids){
+#   print(Busco_id)
+#   tmp = read.table(paste0("https://v10.orthodb.org/tab?query=", Busco_id,"&level=&species=&universal=&singlecopy="), sep = "\t", header = TRUE, quote = "")
+#   Gene = tmp$pub_gene_id[tmp$organism_name=="Gallus gallus"]
+#   GenesNames = c(GenesNames, Gene)
+# }
+
+# Loop over Busco IDs to extract all possible names of this gene
+GenesNamesAll = c()
 for(Busco_id in Busco_ids){
   print(Busco_id)
-  tmp <- read.table(paste0("https://v10.orthodb.org/tab?query=", Busco_id,"&level=&species=&universal=&singlecopy="), sep = "\t", header = TRUE, quote = "")
-  Gene = tmp$pub_gene_id[tmp$organism_name=="Gallus gallus"]
-  GenesNames = c(GenesNames, Gene)
+  tmp = read.table(paste0("https://v10.orthodb.org/tab?query=", Busco_id,"&level=&species=&universal=&singlecopy="), sep = "\t", header = TRUE, quote = "")
+  Gene = unique(tmp$pub_gene_id)
+  GenesNamesAll = c(GenesNamesAll, Gene)
 }
 
-GenesNames
+#### Identify the genes present in our assemby ####
 
+# Load the file
+GenesBarnOwl = read.table("0_DATA/GCF_018691265.1_T.alba_DEE_v4.0_feature_table_genes.txt", sep = "\t", quote = "")
 
+# BUSCOgenesBarnOwl = GenesNames[GenesNames%in%GenesBarnOwl$V15]
+BUSCOgenesBarnOwl = GenesNamesAll[GenesNamesAll%in%GenesBarnOwl$V15]
+
+TabBUSCO_Owl = GenesBarnOwl[GenesBarnOwl$V15%in%BUSCOgenesBarnOwl,c(15, 7, 8, 9)]
+colnames(TabBUSCO_Owl) = c("GeneID", "Chr", "Start", "Stop")
+
+write.table(TabBUSCO_Owl, "BUSCOgenesBarnOwl.txt", row.names = FALSE, quote = FALSE)
+
+####  ####
 
